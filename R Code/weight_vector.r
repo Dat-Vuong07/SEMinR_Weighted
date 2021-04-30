56569a8a838c78b0e99cfc8923035e0fb3e934f1
Dat Vuong <dat.vuong007@gmail.com>
2021-04-30 15:55:00 +0200

    Add R codes

---
+++ R Code/weight_vector.r
@@ -0,0 +1,5 @@
+library(tidyverse)
+weight_vector <- read_csv("/Users/vuongdat/OneDrive - Aarhus universitet/Research/Literature review/Master/Master Project/Distribution_Weighted.csv") %>% pull()
+#weight_vector <- read_csv("/Users/vuongdat/OneDrive - Aarhus universitet/Research/Literature review/Master/Master Project/Distribution_Weighted_6710.csv") %>% pull()
+
+#weight_vector = c(rep(1,250))
