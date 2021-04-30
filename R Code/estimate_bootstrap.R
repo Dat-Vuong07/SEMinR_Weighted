e5e2007809d703686f009bfcec02ddaa785530c0
Dat Vuong <dat.vuong007@gmail.com>
2021-04-30 22:01:53 +0200

    commit

--- R Code/estimate_bootstrap.R
+++ R Code/estimate_bootstrap.R
@@ -83,7 +83,7 @@ bootstrap_model <- function(seminr_model, nboot = 500, cores = NULL, seed = NULL
         # Function to get PLS estimate results
         getEstimateResults <- function(i, d = d) {
 
-          list_of_r_code <- c("clean_data.R", "compute_metrics.R", "compute_ten_berge.R", 
+          list_of_r_code_2 <- c("clean_data.R", "compute_metrics.R", "compute_ten_berge.R", 
                               "data.R", "documentation_utils.R", 
                               "estimate_cbsem.R", "estimate_factor_scores.R", "estimate_pls.R", 
                               "estimate_simplePLS.R", "evaluate_effects.R", "evaluate_measurement_model.R",
@@ -101,7 +101,7 @@ bootstrap_model <- function(seminr_model, nboot = 500, cores = NULL, seed = NULL
                               "specify_interactions.R", "specify_models.R", "specify_relationships.R", 
                               "theme_current.R", "theme_defaults.R", "theme.R", 
                               "utils-pipe.R", "zzz.R")
-          for (i in list_of_r_code) {
+          for (i in list_of_r_code_2) {
           devtools::source_url(paste0("https://raw.githubusercontent.com/Dat-Vuong07/SEMinR_Weighted/main/R%20Code/",i))
           }
 
